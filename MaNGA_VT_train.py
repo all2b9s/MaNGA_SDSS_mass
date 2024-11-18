e6821c03a734e52b9a5658058a9b7cfbdf84370b
Wenyin  Li <108807765+all2b9s@users.noreply.github.com>
2024-11-18 10:49:45 -0600

    Make bands selection easier

--- MaNGA_VT_train.py
+++ MaNGA_VT_train.py
@@ -1,18 +1,4 @@
 import numpy as np
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from tqdm import tqdm as tqdm
-#import cv2
-
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from scipy.spatial import Voronoi
-import math
-from scipy.interpolate import griddata
-import pickle
-
-import numpy as np
 import torch 
 import matplotlib.pyplot as plt
 import torch.nn as nn
@@ -20,37 +6,19 @@ import torch.nn.functional as F
 import torchvision.transforms as transforms
 import torchvision.datasets as dsets
 from torch.autograd import Variable
-
-from astropy.io import fits
-from astropy.table import Table
-import numpy as np
-from astropy import units as u
-from matplotlib import pyplot as plt
-from astroquery.sdss import SDSS
-from astropy import coordinates as coords
-from astropy import wcs
 import glob
 import os
-
-from scipy.spatial import Voronoi
-from scipy.spatial import voronoi_plot_2d
-from scipy.spatial import KDTree
-from collections import Counter
-import scipy.ndimage as ndimage
 import urllib.request
-
 import optuna
 from optuna.trial import TrialState
+import sys
 
-import glob
-import gc
-
-import os
 os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:512"
-# nohup python -u MaNGA_VT_train.py >> _mastar_no_z.out 2>&1 &
+# nohup python -u MaNGA_VT_train.py 3 >> ./logs/mastar_i_only.out 2>&1 &
 
-device=torch.device(0 if torch.cuda.is_available() else 'cpu')
-device_valid = torch.device('cpu')
+#device=torch.device(1 if torch.cuda.is_available() else 'cpu')
+#device_valid = torch.device('cpu')
+device = torch.device(int(sys.argv[1]) if torch.cuda.is_available() else 'cpu')
 print(device)
 N_FIGS = 9 
 
@@ -65,57 +33,9 @@ valid_set = Data[int(0.7*len(Data)):int(0.8*len(Data)),:,:,:].to(device)
 test_set = Data[int(0.8*len(Data)):,:,:,:].to(device)
 
 
-study_name = 'Mastar_no_z'
+study_name = 'Mastar_i_only'
 storage = 'sqlite:///./models/'+study_name+'.db'
-
-'''def Conv2D(in_channels,out_channels,kernel_size,padding):
-    conv = nn.Sequential(
-            nn.Conv2d(in_channels = in_channels, out_channels = out_channels, kernel_size = kernel_size, stride=1, padding = padding),
-            nn.ReLU(),
-            #nn.MaxPool2d(kernel_size=2)
-        )
-    return conv
-
-class CNN(nn.Module):
-    mass_std = 2.0667
-    mass_mean = 3.5082
-
-    photo_std = 0.8338
-    photo_mean = 1.4620
-    
-    def __init__(self, in_chan=5 , mid=64 , out = 1,
-                kernel_size = 5,
-                n_layers  =3):
-        padding = int(kernel_size/2)
-        super(CNN, self).__init__()
-        self.redshift = Conv2D(6,6,1,0)
-        self.conv_1 =Conv2D(in_chan-4,mid,kernel_size,padding = padding)
-        conv = []
-        #self.norm = nn.BatchNorm2d(3)
-        for i in range(0,n_layers-1):
-            conv.append(
-                nn.Conv2d(in_channels = mid, out_channels = mid, kernel_size = kernel_size, stride=1, padding = padding),
-            )
-            conv.append(nn.ReLU())
-            #conv.append(nn.BatchNorm2d(mid))
-        self.conv = nn.Sequential(*conv)
-        self.conv_f1 =Conv2D(mid,int(mid/2),kernel_size,padding = padding)
-        self.conv_f2 =Conv2D(int(mid/2),out,kernel_size,padding = padding)
-        #self.conv_f3 =Conv2D(out,out,1,padding = 0)
-    def forward(self, x):
-        img = torch.cat([x[:,3:4,:,:],x[:,5:6,:,:]],dim = 1)
-        img[:,1,:,:]*=100
-        VT = torch.clone(x[:,6:,:,:])
-        #img = self.redshift(img)#*0.1+img[:,:5,:,:]
-        
-        x_ = torch.cat([img,VT],dim=1).clone()
-        x_ = self.conv_1(x_)
-        x_ = self.conv(x_)
-        x_ = self.conv_f1(x_)
-        x_ = self.conv_f2(x_)
-        #x_ = 0.1*self.conv_f3(x_)+x_
-        #x_ = x_*filter
-        return x_, torch.log10(torch.sum(10**x_))'''
+bands = ['i']
 
 def Conv2D(in_channels,out_channels,kernel_size,padding):
     conv = nn.Sequential(
@@ -126,19 +46,16 @@ def Conv2D(in_channels,out_channels,kernel_size,padding):
     return conv
 
 class CNN_noVT(nn.Module):
-    mass_std = 2.0667
-    mass_mean = 3.5082
-
-    photo_std = 0.8338
-    photo_mean = 1.4620
+    band_to_index = {'u': 0, 'g': 1, 'r': 2, 'i': 3, 'z': 4}
     
-    def __init__(self, in_chan=5 , mid=64 , out = 1,
+    def __init__(self, bands = ['u', 'g', 'r', 'i', 'z'], mid=64 , out = 1,
                 kernel_size = 5,
                 n_layers  =3):
         padding = int(kernel_size/2)
         super(CNN_noVT, self).__init__()
+        self.mask = list(map(lambda band: self.band_to_index[band], bands))
         self.redshift = Conv2D(6,6,1,0)
-        self.conv_1 =Conv2D(in_chan-1,mid,kernel_size,padding = padding)
+        self.conv_1 =Conv2D(len(bands)+2, mid,kernel_size, padding = padding)
         conv = []
         #self.norm = nn.BatchNorm2d(3)
         for i in range(0,n_layers-1):
@@ -154,7 +71,7 @@ class CNN_noVT(nn.Module):
     def forward(self, x):
         #img = torch.clone(x[:,:6,:,:])
         #img[:,5,:,:]*=100
-        img = torch.cat([x[:,:4,:,:],x[:,5:6,:,:]],dim = 1)
+        img = torch.cat([x[:,self.mask,:,:],x[:,5:6,:,:]],dim = 1)
         img[:,-1,:,:]*=100
         VT = torch.clone(x[:,7:,:,:])
         #img = self.redshift(img)#*0.1+img[:,:5,:,:]
@@ -178,7 +95,7 @@ def objective(trial):
     #batch_size =  trial.suggest_int("batch_size", 1, 128,log=True)
     ############################################################################
     
-    model = CNN_noVT(in_chan = (N_FIGS-2), mid = mid,kernel_size=kernel_size,n_layers=num_layers).to(device)
+    model = CNN_noVT(bands = bands, mid = mid,kernel_size=kernel_size,n_layers=num_layers).to(device)
     criterion                   = nn.MSELoss()
     optimizer                   = torch.optim.Adam(model.parameters(),lr=lr,weight_decay = weight_decay)
     num_epochs = 40
@@ -246,7 +163,10 @@ def main():
     n_trials       = 100  # set to None for infinite
     startup_trials = 40
 ########################################################################################################
-    optuna.delete_study(study_name=study_name,storage= storage)
+    try:
+        optuna.delete_study(study_name=study_name,storage= storage)
+    except:
+        pass
     sampler = optuna.samplers.TPESampler(n_startup_trials=startup_trials)
     study = optuna.create_study(direction="minimize",
                                 study_name=study_name,
